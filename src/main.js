e8f517900b12ccfa847e002a8ad3b65d27952aec
Edwinmantilla182 <edwin.jmantilla@gmail.com>
2020-12-15 20:57:07 -0500

    noveno commit

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import vueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import router from './router'
+
 Vue.use(vueRouter)
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /* eslint-disable no-new */
 new Vue({
